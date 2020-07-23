bd5dc3303bb26cc261c9af7d30a5b791f68d3300
Marta Kruszewska <martakruszewska007@gmail.com>
2020-07-23 20:04:36 +0200

    Add writer function

--- micro_crm.py
+++ micro_crm.py
@@ -7,7 +7,7 @@ def main():
     with open('base_micro.csv', 'r', encoding='cp852') as data_base:
         csv_reader = csv.DictReader(data_base, delimiter=';')
 
-        with open('new_base_micro.csv', 'w', encoding='utf8') as new_data:
+        with open('base_micro.csv', 'w', encoding='utf8') as new_data:
             fieldnames = ['ID', 'MEETING_DATE', 'FIRST_NAME', 'LAST_NAME', 'CITY', 'STREET', 'HOME_NUMBER', 'POST_CODE', 'POST_CITY', 'PHONE', 'COMMUNITY', 'COUNTY', 'OFFER', 'WYPOSAŻENIE', 'CENA', 'SPOSÓB ZAKUPU', 'STATUS ', 'RAITING','MEETING_DESCRIPTION', 'NEXT_MEETING' ]
             csv_writer= csv.DictWriter(new_data, fieldnames=fieldnames, delimiter='\t')
             csv_writer.writeheader()
